8620ff468c87433153f9574157f7e9fe4b49648c
johnmark2313 <tolentinojohnmark999@gmail.com>
2023-11-13 17:18:17 +0800

    done in office

--- main/exec/insert.php
+++ main/exec/insert.php
@@ -1,8 +1,27 @@
 <?php
+
+include "../includes/db.php";
+
+
 if(isset($_POST['action'])){
 
     if($_POST['action'] == 'add_log'){
-        echo json_encode('1');
+        
+        if( isset($_POST['Log_Title']) && 
+        isset($_POST['Log_Description'])){
+
+            $log_title = $_POST['Log_Title'];
+            $Log_Description = $_POST['Log_Description'];
+
+            $query = "INSERT INTO logs_table (logs_title, logs_details)";
+            $query .= "VALUES ('$log_title', '$Log_Description')";
+
+            $insert  = mysqli_query ($con,$query);
+
+            if ($insert){
+                echo json_encode("1");
+            }
+        }
     }
 
     
---
+++ main/includes/db.php
@@ -0,0 +1,10 @@
+<?php
+	
+	// ============ HR Admin Database ============
+		$servername1 	= "localhost";
+		$username1 		= "root";
+		$password1 		= "";
+		$dbname1 		= "Logs_db";
+
+		$con = mysqli_connect($servername1, $username1, $password1, $dbname1);
+	// ============ HR Admin Database END ========
